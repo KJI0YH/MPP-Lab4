f99db9bc6d28cc272ba17f64efef5fd6d6f1dc8e
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-11-12 16:19:33 +0300

    add class creator, fix usings, fix method count, delete trivia

--- Core/TestsGenerator.cs
+++ Core/TestsGenerator.cs
@@ -1,5 +1,4 @@
-﻿using Dataflow;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
@@ -8,75 +7,129 @@ namespace Core
 {
     public class TestsGenerator
     {
-        public IEnumerable<FileWithContent> Generate(string source)
+        public string Generate(string source)
         {
-
             CompilationUnitSyntax root = CSharpSyntaxTree.ParseText(source).GetCompilationUnitRoot();
 
-            var usings = root.DescendantNodes().OfType<UsingDirectiveSyntax>();
+            // Generate usings
+            var sourceNamespaces = root.DescendantNodes().OfType<NamespaceDeclarationSyntax>();
+            var sourceUsings = root.DescendantNodes().OfType<UsingDirectiveSyntax>();
+            var usings = new SyntaxList<UsingDirectiveSyntax>(sourceUsings)
+                .Add(UsingDirective(ParseName("System")))
+                .Add(UsingDirective(ParseName("System.Collections.Generic")))
+                .Add(UsingDirective(ParseName("System.Linq")))
+                .Add(UsingDirective(ParseName("System.Text")))
+                .Add(UsingDirective(ParseName("NUnit.Framework")))
+                .AddRange(sourceNamespaces.Select(GetClassUsing));
+
+            // Get source classes
             var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>()
                 .Where(@class => @class.Modifiers.Any(SyntaxKind.PublicKeyword))
                 .Where(@class => !@class.Modifiers.Any(SyntaxKind.StaticKeyword)).ToList();
 
-            return classes.Select(CreateTestClass).ToList();
+            // Generate test classes
+            var members = classes.Select(CreateTestClass).ToArray();
+
+            // Generate unit
+            var unit = CompilationUnit()
+                .WithUsings(usings)
+                .AddMembers(members);
+
+            return unit.NormalizeWhitespace().ToFullString();
         }
 
-        private FileWithContent CreateTestClass(ClassDeclarationSyntax classDeclaration)
+        // Generate using from namespace
+        private UsingDirectiveSyntax GetClassUsing(NamespaceDeclarationSyntax namespaceDeclaration)
         {
-            var unit = SyntaxFactory.CompilationUnit();
-            var methods = CreateTestMethods(classDeclaration);
-            unit.AddMembers(methods);
+            return UsingDirective(namespaceDeclaration.Name);
+        }
 
-            return new FileWithContent("a", unit.NormalizeWhitespace().ToFullString());
+        private MemberDeclarationSyntax CreateTestClass(ClassDeclarationSyntax classDeclaration)
+        {
+            // Generate attributes
+            var attribute = SingletonList(AttributeList(SingletonSeparatedList(
+                Attribute(IdentifierName("TestFixture")))));
+
+            // Generate namespace 
+            var @namespace = NamespaceDeclaration(IdentifierName("Tests"));
+            var ns = classDeclaration.Parent as NamespaceDeclarationSyntax;
+            if (ns != null)
+            {
+                @namespace = NamespaceDeclaration(QualifiedName(ns.Name, IdentifierName("Tests")));
+            }
+
+            // Generate modifiers
+            var modifiers = TokenList(Token(SyntaxKind.PublicKeyword));
+
+            // Generate class methods
+            var methods = CreateTestMethods(classDeclaration);
+
+            // Generate class
+            var testClass = ClassDeclaration(classDeclaration.Identifier.Text + "_Tests")
+                .WithAttributeLists(attribute)
+                .WithModifiers(modifiers)
+                .AddMembers(methods);
+
+            // Add test class to namespace
+            @namespace = @namespace.AddMembers(testClass);
+
+            return @namespace;
         }
 
         private MemberDeclarationSyntax[] CreateTestMethods(SyntaxNode syntaxNode)
         {
-            var methods = syntaxNode.DescendantNodes().OfType<MethodDeclarationSyntax>()
-                .Where(method => method.Modifiers.Any(SyntaxKind.PublicKeyword));
-
-            return methods.Select(CreateTestMethod).ToArray();
-        }
-
-        private MemberDeclarationSyntax CreateTestMethod(MethodDeclarationSyntax method)
-        {
             // Generate attributes
             var attribute = SingletonList(AttributeList(SingletonSeparatedList(
-                Attribute(IdentifierName("Test"))))
-                .WithCloseBracketToken(Token(TriviaList(), SyntaxKind.CloseBracketToken, TriviaList(CarriageReturnLineFeed))));
+                Attribute(IdentifierName("Test")))));
 
             // Generate modifiers
-            var modifiers = TokenList(Token(TriviaList(), SyntaxKind.PublicKeyword, TriviaList(Space)));
+            var modifiers = TokenList(Token(SyntaxKind.PublicKeyword));
 
             // Generate return type
-            var returnType = PredefinedType(Token(TriviaList(), SyntaxKind.VoidKeyword, TriviaList()));
-
-            // Generate identifier
-            var identifier = Identifier(TriviaList(SyntaxFactory.Space), method.Identifier.Text + "_Test", TriviaList());
+            var returnType = PredefinedType(Token(SyntaxKind.VoidKeyword));
 
             // Generate body
             var body = Block(ExpressionStatement(
                 InvocationExpression(
                     MemberAccessExpression(
-                        SyntaxKind.SimpleMemberAccessExpression, IdentifierName(Identifier(TriviaList(Tab), "Assert", TriviaList())), IdentifierName("Fail")))
+                        SyntaxKind.SimpleMemberAccessExpression, IdentifierName(Identifier("Assert")), IdentifierName("Fail")))
                 .WithArgumentList(ArgumentList(SingletonSeparatedList(
-                    Argument(LiteralExpression(SyntaxKind.StringLiteralExpression, Literal("autogenerated"))))))))
-                .WithOpenBraceToken(Token(TriviaList(), SyntaxKind.OpenBraceToken, TriviaList(CarriageReturnLineFeed)))
-                .WithCloseBraceToken(Token(TriviaList(CarriageReturnLineFeed), SyntaxKind.CloseBraceToken, TriviaList()));
+                    Argument(LiteralExpression(SyntaxKind.StringLiteralExpression, Literal("autogenerated"))))))));
 
-            // Generate test method
-            var unit = SyntaxFactory.MethodDeclaration(returnType, identifier)
-                .WithAttributeLists(attribute)
-                .WithModifiers(modifiers)
-                .WithParameterList(method.ParameterList)
-                .WithBody(body);
+            var result = new List<MemberDeclarationSyntax>();
+            var methods = syntaxNode.DescendantNodes().OfType<MethodDeclarationSyntax>()
+                .Where(method => method.Modifiers.Any(SyntaxKind.PublicKeyword)).ToList();
 
+            methods.Sort(
+                (method1, method2) => string.Compare(method1.Identifier.Text, method2.Identifier.Text, StringComparison.Ordinal));
 
-            var str = unit.ToFullString();
+            int IdIndex = -1;
+            for (int i = 0; i < methods.Count; i++)
+            {
+                // Generate methods identificator
+                if (i != methods.Count - 1 && methods[i].Identifier.Text == methods[i + 1].Identifier.Text)
+                {
+                    IdIndex = 0;
+                }
+                else if (i != 0 && methods[i].Identifier.Text == methods[i - 1].Identifier.Text)
+                {
+                    IdIndex++;
+                }
+                else
+                {
+                    IdIndex = -1;
+                }
+                string identificator = methods[i].Identifier.Text + (IdIndex == -1 ? "" : IdIndex.ToString()) + "_Test";
 
-            return unit;
+                // Generate methods
+                result.Add(MethodDeclaration(returnType, identificator)
+                    .WithAttributeLists(attribute)
+                    .WithModifiers(modifiers)
+                    .WithParameterList(methods[i].ParameterList)
+                    .WithBody(body));
+            }
+
+            return result.ToArray();
         }
-
-
     }
 }
