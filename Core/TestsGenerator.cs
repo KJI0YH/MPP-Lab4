02050cf7eddb371d69b676a518c14410e631057c
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2022-11-22 18:30:29 +0300

    merge fix

--- Core/TestsGenerator.cs
+++ Core/TestsGenerator.cs
@@ -1,4 +1,9 @@
-﻿namespace Core
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Core
 {
     public class TestsGenerator
     {
